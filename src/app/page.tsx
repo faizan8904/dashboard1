1556c4b8aa1a8c0d6662dc724bab13f01473c20d
faizan8904 <faizansyed8904@gmail.com>
2025-07-16 22:01:14 +0530

    first setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Backpack } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    Children
+   </div>
+  );
+}
+ 
